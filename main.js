2fd0b53d04e453fcf07c72e591b0dd57d8fe8ab9
Nunes <GilsonNunes_dev@outlook.com>
2020-12-20 21:25:46 -0300

    adicionado script com lógica do projeto

---
+++ main.js
@@ -0,0 +1,33 @@
+var apiKey = {
+  key: "sua api key do coinmarketcap",
+};
+
+fetch(
+  `https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=${apiKey.key}`
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        "Erro ao executar a requisição, status " + response.status
+      );
+    }
+    return response.json();
+  })
+
+  .then((api) => {
+    let texto = "";
+    for (let i = 0; i < 10; i++) {
+      texto += `
+        <div class="media">
+            <img src="coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+            <div class="media-body">
+            <h5 class="mt-2">${api.data[i].name}</h5>
+            <p>${api.data[i].symbol}</p>
+            </div>
+        </div>
+        `;
+      document.getElementById("coins").innerHTML = texto;
+    }
+  })
+  .catch((error) => {
+  });
